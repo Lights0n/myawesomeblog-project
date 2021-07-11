847e936bf03e54b301fc2e61772afd6cb9f5dd84
EL Studente <stasi1357@mail.ru>
2021-07-11 13:22:40 +0300

    add post moddel

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+	blog_title = models.CharField(max_length=300)
+	blog_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+	blog_text = models.TextField(max_length=300)
+	blog_image = models.ImageField(upload_to='blog_images/')
+	
